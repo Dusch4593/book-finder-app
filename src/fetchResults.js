3616840b6a4be8142cc71714795e301c5c8cc37a
Brandon Dusch <brandondusch@Brandons-MacBook-Pro.local>
2019-12-03 14:35:40 -0500

    modified .gitignore file

---
+++ src/fetchResults.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const extractData = (response) => {
+
+  if(!response) return {};
+  const data = response.results.work;
+  return data.map((book) => {
+    const title = book.best_book.title._text;
+    const author = book.best_book.author.name._text;
+    const imgLink = book.best_book.image_url;
+
+    return {
+      title,
+      author,
+      imgLink
+    };
+
+
+  });
+};
+
+export default function fetchReultsAlt(search_query) {
+  const GOOD_READS_API_URL = "http://localhost:3000/api/search?q=" + search_query;
+  return fetch(GOOD_READS_API_URL)
+              .then((res) => res.json())
+              .then(extractData)
+              .then((data) => {return data})
+              .catch(console.log)
+
+}
