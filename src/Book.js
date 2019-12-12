201c1e18f356bd419a39e5c70fd0017e3a72a29b
Brandon Dusch <brandondusch@Brandons-MacBook-Pro.local>
2019-12-11 23:43:18 -0500

    Book covers now hyperlinked to page on Goodreads

--- src/Book.js
+++ src/Book.js
@@ -9,13 +9,16 @@ export default class Book extends React.Component {
       pubDate
     } = this.props;
 
+    const bookLink = `https://www.goodreads.com/book/title?id=${title}`;
     return (
       <div className = "book">
         <div className = "book_title">
           <h2>{title}</h2>
         </div>
         <div className = "book_image_block">
-          <img src={imgLink} alt = '' className = "book_image" />
+          <a target="_blank" rel="noopener noreferrer" href={bookLink}>
+            <img src={imgLink} alt = '' className = "book_image" />
+          </a>
         </div>
         <div className = "book_info">
         <strong>Author</strong>: {author} <br/><br/>
